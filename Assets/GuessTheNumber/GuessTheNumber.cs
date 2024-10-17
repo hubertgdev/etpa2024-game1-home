28b3044d9b7e9e46b2e62284a72a8bac3f64c20c
Hubert Grossin <hubert.grossin@gmail.com>
2024-10-17 12:55:47 +0200

    Add GuessTheNumber

---
+++ Assets/GuessTheNumber/GuessTheNumber.cs
@@ -0,0 +1,56 @@
+using TMPro;
+using UnityEngine;
+
+public class GuessTheNumber : MonoBehaviour
+{
+
+    public TMP_Text messageText;
+    public TMP_InputField numberInput;
+
+    private int randomNumber;
+
+    private void Start()
+    {
+        ResetGame();
+    }
+
+    public void Try()
+    {
+        if (string.IsNullOrWhiteSpace(numberInput.text))
+        {
+            messageText.text = "Please enter a valid number.";
+            numberInput.text = "";
+            return;
+        }
+
+        if(int.TryParse(numberInput.text, out int playerNumber))
+        {
+            if (playerNumber == randomNumber)
+            {
+                messageText.text = "VICTORY!";
+            }
+            else if (playerNumber > randomNumber)
+            {
+                messageText.text = "Lower...";
+            }
+            else if (playerNumber < randomNumber)
+            {
+                messageText.text = "Greater...";
+            }
+        }
+        else
+        {
+            messageText.text = "Please enter a valid number.";
+        }
+
+        numberInput.text = "";
+    }
+
+    public void ResetGame()
+    {
+        randomNumber = Random.Range(1, 100 + 1);
+        Debug.Log("Generated number: " + randomNumber);
+        messageText.text = "Guess the number...";
+    }
+
+}
