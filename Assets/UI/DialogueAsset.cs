540b1b8b899f7cf40bd84308ecc2ad176fd79f01
Hubert Grossin <hubert.grossin@gmail.com>
2024-10-24 12:30:57 +0200

    Added dialogue system

---
+++ Assets/UI/DialogueAsset.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DialogueAsset : ScriptableObject
+{
+
+    [TextArea(3, 6)]
+    public string[] dialogue;
+
+}
---
+++ Assets/UI/DialoguePlayer.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class DialoguePlayer : MonoBehaviour
+{
+
+    public DialogueAsset dialogueAsset;
+    public TMP_Text dialogueText;
+    public GameObject dialogueBox;
+
+    private int dialogueLineIndex;
+
+    private void Start()
+    {
+        dialogueText.text = dialogueAsset.dialogue[0];
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //dialogueLineIndex = dialogueLineIndex + 1;
+            //dialogueLineIndex += 1;
+            dialogueLineIndex++;
+
+            if (dialogueLineIndex < dialogueAsset.dialogue.Length)
+            {
+                dialogueText.text = dialogueAsset.dialogue[dialogueLineIndex];
+            }
+            else
+            {
+                dialogueBox.gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
